b2547854f2481d24c3ac99b78d3a61422fd79da2
tesdeve <tesdeve@gmail.com>
2018-05-04 09:51:55 +0100

    Setting host in production for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   #config.secret_key_base = ENV["SECRET_KEY_BASE"]  
+
+  config.action_mailer.default_url_options = { :host => 'listispro.herokuapp.com' }
 end
